b2d3201b5eae9126e5a160b4c0e5b95389a778bf
vpatel2501 <59679882+vpatel2501@users.noreply.github.com>
2025-03-04 18:18:43 -0500

    added firestoreservice

---
+++ frontend/src/app/firebase.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData, addDoc} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  constructor(private firestore: Firestore) {}
+
+  getRecipes(): Observable<any[]> {
+    const recipesRef = collection(this.firestore, 'recipes');
+    return collectionData(recipesRef, { idField: 'id' });
+  }
+}
