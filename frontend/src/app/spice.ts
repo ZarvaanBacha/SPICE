a529055ad7cc54d5c43b38c33ea3bda44321978e
LudoProvost <ludovic.provost8@gmail.com>
2025-03-14 17:10:48 -0400

    added comment about Spice interface

--- frontend/src/app/spice.ts
+++ frontend/src/app/spice.ts
@@ -2,4 +2,4 @@ export interface Spice {
     id: number;
     name: string;
     quantity: number;
-} //TODO : move this to a common model file..
+} //TODO : move this to a common model file and possibly just change its implementation to fit the SpiceContainer interface
