601181b21bdda40a05374fe709e4c8316734707e
vpatel2501 <59679882+vpatel2501@users.noreply.github.com>
2025-03-21 17:41:25 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,12 +11,6 @@ const pyDir = "C:/Users/ludov/Python/python.exe"; //TODO set as path to python.e
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({origin: "http://localhost:4200"}));
-// app.use((res,next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requeested-With, Content-Type, Accept, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   next();
-// })
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
