3141665c471908e5b2de2b2f35260d4822012ac1
vpatel2501 <59679882+vpatel2501@users.noreply.github.com>
2025-03-21 17:41:25 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,12 +7,6 @@ let email = '';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({origin: "http://localhost:4200"}));
-// app.use((res,next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requeested-With, Content-Type, Accept, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   next();
-// })
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
