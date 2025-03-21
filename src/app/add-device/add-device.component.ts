9b442943db0d482d52e10afdcced664d29607f14
vpatel2501 <59679882+vpatel2501@users.noreply.github.com>
2025-03-20 22:34:35 -0400

    Update add-device.component.ts

--- src/app/add-device/add-device.component.ts
+++ src/app/add-device/add-device.component.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { DeviceModel } from '../shared/deviceModel'
 import { FirebaseRecipeService } from '../firebase-recipe.service';
 
+import { NgIf } from '@angular/common';
+import { FirebaseMainService } from '../firebase-main.service';
 
 @Component({
   selector: 'app-add-device',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './add-device.component.html',
   styleUrl: './add-device.component.css'
 })
@@ -23,6 +25,21 @@ export class AddDeviceComponent {
       .catch((err) => {
         console.error('Error adding device:', err);
       });
+
+
+
+  // onSubmit(productID: string){
+  //   this.Firebasemain.setProductID(productID)
+  // }
+
+  // resetDevice() {
+  //   this.Firebasemain.setProductID(this.userId, null)
+  //     .then(() => {
+  //       console.log('Device reset successfully');
+  //       this.productID = null;
+  //     })
+  //     .catch(error => console.error('Error resetting device:', error));
+  // }
   }
 
 }
